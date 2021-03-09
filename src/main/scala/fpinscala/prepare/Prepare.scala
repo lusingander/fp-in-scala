106e24d8586778b4aca6f8aa66c3f75e03088434
Kyosuke Fujimoto <kyoro.f@gmail.com>
2021-03-09 22:04:53 +0900

    ex 2.2 - 2.5

--- src/main/scala/fpinscala/prepare/Prepare.scala
+++ src/main/scala/fpinscala/prepare/Prepare.scala
@@ -12,6 +12,7 @@ object Prepare {
     go(n, 1)
   }
 
+  // 2.1
   def fib(n: Int): Int = {
     @tailrec
     def go(n: Int, a: Int, b: Int): Int = {
@@ -29,4 +30,44 @@ object Prepare {
       bad_fib(n - 1) + bad_fib(n - 2)
     }
   }
+
+  def findFirst[A](as: Array[A], pred: A => Boolean): Int = {
+    @tailrec
+    def loop(n: Int): Int = {
+      if (n >= as.length) {
+        -1
+      } else if (pred(as(n))) {
+        n
+      } else {
+        loop(n + 1)
+      }
+    }
+    loop(0)
+  }
+
+  // 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length - 1) {
+        true
+      } else if (!ordered(as(n), as(n + 1))) {
+        false
+      } else {
+        loop(n + 1)
+      }
+    }
+    loop(0)
+  }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = b => f(a, b)
+
+  // 2.3
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => (b => f(a, b))
+
+  // 2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+  // 2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
 }
